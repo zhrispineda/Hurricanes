a218902c2347daced25dbda515b030e9da90bb80
Chris <zhris@zhr.is>
2024-12-07 11:47:52 -0600

    Initial Commit

---
+++ Hurricanes/HurricanesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HurricanesApp.swift
+//  Hurricanes
+//
+//  Created by Chris on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HurricanesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
