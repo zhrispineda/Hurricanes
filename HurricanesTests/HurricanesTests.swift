f14e49cea8478cb0f45c6f54f9d41e014dcf9d0d
Chris <zhris@zhr.is>
2025-02-11 12:39:56 -0500

    Modify testing functionality for image endpoints

--- HurricanesTests/HurricanesTests.swift
+++ HurricanesTests/HurricanesTests.swift
@@ -7,59 +7,16 @@ import Testing
 @testable import Hurricanes
 
 struct HurricanesTests {
-    // MARK: - 7 day and 2 day image endpoints
-    
-    // 2-day Atlantic Outlook
-    @Test func twoDayAtlanticOutlookImage() async throws {
+    // MARK: Test image endpoints from NOAA
+    @Test(arguments: [
+        "https://www.nhc.noaa.gov/xgtwo/two_atl_7d0.png", // 7-day Atlantic Outlook
+        "https://www.nhc.noaa.gov/xgtwo/two_atl_2d0.png", // 2-day Atlantic Outlook
+        "https://www.nhc.noaa.gov/xgtwo/two_pac_2d0.png", // 2-day Pacific Outlook
+        "https://www.nhc.noaa.gov/xgtwo/two_cpac_2d0.png", // 2-day Central Pacific Outlook
+    ])
+    func imageEndpointsExist(endpointURL: String) async throws {
         let hurricanesHelper = HurricanesHelper()
-        
-        if let uiImage = await hurricanesHelper.getImage(from: "https://www.nhc.noaa.gov/xgtwo/two_atl_2d0.png") {
-            #expect(uiImage != nil)
-        }
-    }
-    
-    // 2-day Central Pacific Outlook
-    @Test func twoDayCentralPacificOutlookImage() async throws {
-        let hurricanesHelper = HurricanesHelper()
-        
-        if let uiImage = await hurricanesHelper.getImage(from: "https://www.nhc.noaa.gov/xgtwo/two_cpac_2d0.png") {
-            #expect(uiImage != nil)
-        }
-    }
-    
-    // 2-day Eastern Pacific Outlook
-    @Test func twoDayEasternPacificOutlookImage() async throws {
-        let hurricanesHelper = HurricanesHelper()
-        
-        if let uiImage = await hurricanesHelper.getImage(from: "https://www.nhc.noaa.gov/xgtwo/two_pac_2d0.png") {
-            #expect(uiImage != nil)
-        }
-    }
-    
-    // 7-day Atlantic Outlook
-    @Test func sevenDayAtlanticOutlookImage() async throws {
-        let hurricanesHelper = HurricanesHelper()
-        
-        if let uiImage = await hurricanesHelper.getImage(from: "https://www.nhc.noaa.gov/xgtwo/two_atl_7d0.png") {
-            #expect(uiImage != nil)
-        }
-    }
-    
-    // 7-day Central Pacific Outlook
-    @Test func sevenDayCentralPacificOutlookImage() async throws {
-        let hurricanesHelper = HurricanesHelper()
-        
-        if let uiImage = await hurricanesHelper.getImage(from: "https://www.nhc.noaa.gov/xgtwo/two_cpac_2d0.png") {
-            #expect(uiImage != nil)
-        }
-    }
-    
-    // 7-day Eastern Pacific Outlook
-    @Test func sevenDayEasternPacificOutlookImage() async throws {
-        let hurricanesHelper = HurricanesHelper()
-        
-        if let uiImage = await hurricanesHelper.getImage(from: "https://www.nhc.noaa.gov/xgtwo/two_pac_2d0.png") {
-            #expect(uiImage != nil)
-        }
+        let img = await hurricanesHelper.getImage(from: endpointURL)
+        #expect(img != nil)
     }
 }
