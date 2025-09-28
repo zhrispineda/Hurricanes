09a4421f19488fd66355a5327acc2413e03a824d
zhrispineda <zhris@zhr.is>
2025-09-28 12:56:41 -0500

    Update PFAnimatedImage to add PFAnimatedImageLoader

--- Hurricanes/Components/PFAnimatedImage.swift
+++ Hurricanes/Components/PFAnimatedImage.swift
@@ -6,32 +6,61 @@
 import SwiftUI
 
 struct PFAnimatedImage: UIViewRepresentable {
-    let link: String
+    let data: Data
     
-    init(_ link: String) {
-        self.link = link
+    init(data: Data) {
+        self.data = data
     }
-
+    
     func makeUIView(context: Context) -> UIView {
         let path = "/System/Library/PrivateFrameworks/PhotosPlayer.framework/PhotosPlayer"
         dlopen(path, RTLD_NOW)
-
-        guard let gifURL = URL(string: link), let gifData = try? Data(contentsOf: gifURL) else {
-            return UIView()
-        }
-
+        
         let animClass = NSClassFromString("PFAnimatedImage") as! NSObject.Type
         let animInstance = animClass.perform(NSSelectorFromString("alloc"))!.takeUnretainedValue()
-        _ = animInstance.perform(NSSelectorFromString("initWithData:"), with: gifData)
-
+        _ = animInstance.perform(NSSelectorFromString("initWithData:"), with: data)
+        
         let controller = NSClassFromString("ISAnimatedImageView") as! UIView.Type
         let controllerInstance = controller.perform(NSSelectorFromString("alloc"))!.takeUnretainedValue()
         let instance = controllerInstance.perform(NSSelectorFromString("initWithAnimatedImage:"), with: animInstance).takeUnretainedValue() as! UIView
-
+        
         instance.setValue(true, forKey: "playing")
         
         return instance
     }
-
+    
     func updateUIView(_ uiView: UIView, context: Context) {}
+}
+
+struct PFAnimatedImageLoader: View {
+    let link: String
+    @State private var data: Data? = nil
+    @State private var isLoading = true
+    
+    var body: some View {
+        Group {
+            if let data = data {
+                PFAnimatedImage(data: data)
+            } else if isLoading {
+                ProgressView("Loading…")
+                    .frame(width: 100)
+            } else {
+                ContentUnavailableView("Failed to load", systemImage: "exclamationmark.triangle.fill")
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    private func loadData() async {
+        do {
+            guard let url = URL(string: link) else { return }
+            let (fetchedData, _) = try await URLSession.shared.data(from: url)
+            self.data = fetchedData
+        } catch {
+            print("Failed to fetch GIF from URL:", error)
+        }
+        self.isLoading = false
+    }
 }
--- Hurricanes/Views/SatellitesView.swift
+++ Hurricanes/Views/SatellitesView.swift
@@ -9,16 +9,25 @@ struct SatellitesView: View {
     var body: some View {
         NavigationStack {
             List {
-                Section {
-                    PFAnimatedImage("https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/GEOCOLOR/GOES16-TAW-GEOCOLOR-900x540.gif")
+                Section("Tropical Atlantic GEOCOLOR") {
+                    PFAnimatedImageLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/GEOCOLOR/GOES16-TAW-GEOCOLOR-900x540.gif")
                         .aspectRatio(900/540, contentMode: .fit)
-                } header: {
-                    Text("Atlantic GEOCOLOR")
+                }
+                .sectionHeaderStyle()
+                
+                Section("Atlantic Coast GEOCOLOR") {
+                    PFAnimatedImageLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/eus/GEOCOLOR/20252711301-20252711711-GOES19-ABI-EUS-GEOCOLOR-1000x1000.gif")
+                        .aspectRatio(1000/1000, contentMode: .fit)
+                }
+                .sectionHeaderStyle()
+                
+                Section("Pacific GEOCOLOR") {
+                    PFAnimatedImageLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/tpw/GEOCOLOR/20252710710-20252711700-GOES18-ABI-TPW-GEOCOLOR-900x540.gif")
+                        .aspectRatio(900/540, contentMode: .fit)
                 }
                 .sectionHeaderStyle()
             }
             .navigationTitle("Satellites")
-            .listStyle(.inset)
         }
     }
 }
