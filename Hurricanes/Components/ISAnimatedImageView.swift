5ca36c3f620f0da05eaef58f2c2e0d17f3d5f08b
zhrispineda <zhris@zhr.is>
2025-10-03 11:27:09 -0400

    Update SatelliteView, ISAnimatedImageView for AnimatedImageLoader

--- Hurricanes/Components/ISAnimatedImageView.swift
+++ Hurricanes/Components/ISAnimatedImageView.swift
@@ -5,9 +5,9 @@
 
 import SwiftUI
 
-/// Returns a `UIView` based on `ISAnimatedImageView` and animation image data as `PFAnimatedImage`.
+/// Returns a `UIView` based on `ISAnimatedImageView` with animated image data as `PFAnimatedImage`.
 ///
-/// - Warning: Do not use this method for loading animated images as it is private. Consider other public alternatives.
+/// - Warning: Do not use this method as it is private. Consider a public alternative.
 ///
 /// Both view and class originate from private framework `PhotosPlayer`.
 ///
@@ -38,36 +38,69 @@ struct ISAnimatedImageView: UIViewRepresentable {
     func updateUIView(_ uiView: UIView, context: Context) {}
 }
 
-/// - Warning: Do not use this method for loading animated images as it is private. Consider other public alternatives.
+/// - Warning: Do not use this method for loading animated images as it is private. Consider a public alternative.
 struct ISAnimatedImageViewLoader: View {
+    var imageLoader = AnimatedImageLoader()
     let link: String
-    @State private var data: Data? = nil
-    @State private var isLoading = true
     
     var body: some View {
         Group {
-            if let data = data {
+            if let data = imageLoader.data {
                 ISAnimatedImageView(data: data)
-            } else if isLoading {
-                ProgressView("Loading…")
-                    .frame(width: 100)
-                    .task {
-                        await loadData()
-                    }
-            } else {
+            } else if imageLoader.failed {
                 ContentUnavailableView("Failed to load", systemImage: "exclamationmark.triangle.fill")
+            } else {
+                ProgressView("Loading…")
+                    .frame(width: 150)
+                    .onAppear {
+                        imageLoader.load(from: link)
+                    }
+            }
+        }
+    }
+}
+
+@Observable class AnimatedImageLoader {
+    var data: Data? = nil
+    var isLoading = false
+    var failed = false
+    
+    private var task: Task<Void, Never>? = nil
+    
+    func load(from link: String) {
+        guard data == nil, !isLoading else { return }
+        
+        isLoading = true
+        failed = false
+        
+        task = Task {
+            do {
+                guard let url = URL(string: link) else { return }
+                print("Loading GIF:", link)
+                let (fetchedData, _) = try await URLSession.shared.data(from: url)
+                
+                if !Task.isCancelled {
+                    await MainActor.run {
+                        self.data = fetchedData
+                    }
+                }
+            } catch let error {
+                if !Task.isCancelled {
+                    print("GIF failure: \(error)")
+                    await MainActor.run {
+                        self.failed = true
+                    }
+                }
+            }
+            
+            await MainActor.run {
+                self.isLoading = false
             }
         }
     }
     
-    private func loadData() async {
-        do {
-            guard let url = URL(string: link) else { return }
-            let (fetchedData, _) = try await URLSession.shared.data(from: url)
-            self.data = fetchedData
-        } catch {
-            print("Failed to fetch GIF from URL:", error)
-        }
-        self.isLoading = false
+    func cancel() {
+        task?.cancel()
+        task = nil
     }
 }
--- Hurricanes/Views/SatellitesView.swift
+++ Hurricanes/Views/SatellitesView.swift
@@ -12,18 +12,21 @@ struct SatellitesView: View {
                 Section("Tropical Atlantic GEOCOLOR") {
                     ISAnimatedImageViewLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/taw/GEOCOLOR/GOES16-TAW-GEOCOLOR-900x540.gif")
                         .aspectRatio(900/540, contentMode: .fit)
+                        .frame(maxWidth: .infinity)
                 }
                 .sectionHeaderStyle()
                 
                 Section("Atlantic Coast GEOCOLOR") {
-                    ISAnimatedImageViewLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/eus/GEOCOLOR/20252711301-20252711711-GOES19-ABI-EUS-GEOCOLOR-1000x1000.gif")
+                    ISAnimatedImageViewLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES19/ABI/SECTOR/eus/GEOCOLOR/GOES19-EUS-GEOCOLOR-1000x1000.gif")
                         .aspectRatio(1000/1000, contentMode: .fit)
+                        .frame(maxWidth: .infinity)
                 }
                 .sectionHeaderStyle()
                 
                 Section("Pacific GEOCOLOR") {
-                    ISAnimatedImageViewLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/tpw/GEOCOLOR/20252710710-20252711700-GOES18-ABI-TPW-GEOCOLOR-900x540.gif")
+                    ISAnimatedImageViewLoader(link: "https://cdn.star.nesdis.noaa.gov/GOES18/ABI/SECTOR/tpw/GEOCOLOR/GOES18-TPW-GEOCOLOR-900x540.gif")
                         .aspectRatio(900/540, contentMode: .fit)
+                        .frame(maxWidth: .infinity)
                 }
                 .sectionHeaderStyle()
             }
